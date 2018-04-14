c49b73ef5319ac54099fa77118eaa76ef1debf9f
Christopher DeCarolis <cdguitar817@gmail.com>
2018-04-13 21:16:25 -0400

    small test script for cuda

---
+++ plots/bench_dot_prod_cuda.py
@@ -0,0 +1,20 @@
+import cupy as cp
+import time
+import csv
+import sys
+"""
+Lightweight script that benchmarks the performance of matrix-matrix multiplication.
+Command line arguments: [d, k, num_samples]
+"""
+
+d = int(sys.argv[1])
+num_samples = int(sys.argv[2])
+v1 = cp.full((d), 0.5)
+v2 = cp.full((d), 0.5)
+start = time.time()
+for i in range(0, num_samples):
+    cp.dot(v1, v2)
+end = time.time()
+with open('data/data_dot_prod.csv', 'a') as f:
+    writer = csv.writer(f, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([str(d),str(end-start),str(num_samples)])
